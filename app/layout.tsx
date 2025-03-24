80982f430f972006ee305fae617f4c3e536b0b4d
SumitGarg <garg10sumit@gmail.com>
2025-03-24 23:36:10 +0530

    feat: add desktop hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "@/app/ui/global.css";
+
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
